49c71a03e1fbdd536bf647b3c5ca0e091585ccd7
Terry Martin <terryzmartin@gmail.com>
2013-11-08 13:03:37 -0800

    add apps to mongosync.js

--- bin/mongosync.js
+++ bin/mongosync.js
@@ -1,6 +1,6 @@
 var mongojs = require('mongojs')
 	, redis = require("redis")
-	, config = require('../config/app.json');
+	, config = require('../config');
 
 
 /* 
@@ -11,7 +11,7 @@ var mongojs = require('mongojs')
 
 */
 
-var db = mongojs(config.databases.mongo, ['domains']);
+var db = mongojs(config.databases.mongo, ['domains','apps']);
 
 // Connect to SP redis instance
 var Redis = redis.createClient(
@@ -21,31 +21,51 @@ var Redis = redis.createClient(
 	}
 );
 
+function getDomains(err, doc) {
+			
+	if(doc) {
+		// Save DB info
+		doc.dbEngine = doc.database.engine;
+		doc.dbPort = doc.database.port;
+		doc.dbHost = doc.database.host;
+		doc.dbPass = doc.database.pass;
+		doc.dbUser = doc.database.user;
+		
+		// Add domain to hash table
+		Redis.HMSET(doc.id, doc, function(e,o) {
+			if (e) { console.log(e); return; }
+		});
+	} else {
+		console.log('done with domains');
+	}
+	
+};
+
+function getApps(err, doc) {
+			
+	if(doc) {
+		//delete doc.builds;
+		
+		// clean JSON object
+		var o = JSON.stringify(doc);
+		o = JSON.parse(o);
+		
+		// Add domain to hash table
+		Redis.HMSET(o._id, o, function(e,o) {
+			if (e) { console.log(e); return; }
+		});
+	} else {
+		console.log('done with apps');
+	}
+	
+};
+
 // Select 'domains' db
 Redis.select(0, function() {
 
 	// Run search for all domains on MongoHQ
-	db.domains.find().forEach(function(err, doc) {
-				
-		if(doc) {
-						
-			// Save DB info
-			doc.dbEngine = doc.database.engine;
-			doc.dbPort = doc.database.port;
-			doc.dbHost = doc.database.host;
-			doc.dbPass = doc.database.pass;
-			doc.dbUser = doc.database.user;
-			
-			// Add domain to hash table
-			Redis.HMSET(doc.id, doc, function(e,o) {
-				if (e) { console.log(e); return; }
-				console.log(o);
-			});
-		} else {
-			Redis.BGSAVE();
-			console.log('done');
-		}
-		
-	});
+	db.domains.find().forEach(getDomains);
+	// Run search for all domains on MongoHQ
+	db.apps.find().forEach(getApps);
 	
 });
