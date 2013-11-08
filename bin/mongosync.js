43452458e6381998f80be1d8d04fff14a3711eb0
Terry Martin <terryzmartin@gmail.com>
2013-11-08 14:07:14 -0800

    update mongosync.js to populate domain db with apps

--- bin/mongosync.js
+++ bin/mongosync.js
@@ -21,19 +21,25 @@ var Redis = redis.createClient(
 	}
 );
 
-function getDomains(err, doc) {
+function getDomains(err, domain) {
 			
-	if(doc) {
+	if(domain) {
 		// Save DB info
-		doc.dbEngine = doc.database.engine;
-		doc.dbPort = doc.database.port;
-		doc.dbHost = doc.database.host;
-		doc.dbPass = doc.database.pass;
-		doc.dbUser = doc.database.user;
+		domain.dbEngine = domain.database.engine;
+		domain.dbPort = domain.database.port;
+		domain.dbHost = domain.database.host;
+		domain.dbPass = domain.database.pass;
+		domain.dbUser = domain.database.user;
 		
 		// Add domain to hash table
-		Redis.HMSET(doc.id, doc, function(e,o) {
+		Redis.HMSET(domain.id, domain, function(e,o) {
 			if (e) { console.log(e); return; }
+			
+			// Run search for all domains on MongoHQ
+			db.apps.find({"domain":domain.id}).forEach(function(err, app){
+				getApps(err,app,domain);
+			});
+			
 		});
 	} else {
 		console.log('done with domains');
@@ -41,18 +47,28 @@ function getDomains(err, doc) {
 	
 };
 
-function getApps(err, doc) {
+function getApps(err, app, domain) {
 			
-	if(doc) {
+	if(app) {
 		//delete doc.builds;
-		
+
 		// clean JSON object
-		var o = JSON.stringify(doc);
+		var o = JSON.stringify(app);
 		o = JSON.parse(o);
 		
-		// Add domain to hash table
-		Redis.HMSET(o._id, o, function(e,o) {
-			if (e) { console.log(e); return; }
+		// open connection to domain db
+		console.lo
+		console.log('Connecting to domain db: '+domain.dbHost+':'+domain.dbPort);
+		var Redis2 = redis.createClient(domain.dbPort, domain.dbHost,{'no_ready_check' : true});
+		Redis2.on("error", function (err) {
+			console.log(err.message);
+		});
+		Redis2.select(0, function() {
+			// Save apps to domain db
+			Redis2.HMSET(o.domain+'-'+o._id, o, function(e,o) {
+				if (e) { console.log(e); return; }
+				console.log(o);
+			});
 		});
 	} else {
 		console.log('done with apps');
@@ -65,7 +81,5 @@ Redis.select(0, function() {
 
 	// Run search for all domains on MongoHQ
 	db.domains.find().forEach(getDomains);
-	// Run search for all domains on MongoHQ
-	db.apps.find().forEach(getApps);
 	
 });
